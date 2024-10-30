1aad659246f2203e039231cf34911573001ee385
Luis Parente <lgsp25hotmail.com>
2024-10-30 18:23:51 -0300

    Insert User

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -22,5 +22,9 @@ public class UserService {
 	public User findyId(Long id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.get();
-				}
+	}
+	
+	public User insert(User user) {
+		return repository.save(user);
+	}
 }
